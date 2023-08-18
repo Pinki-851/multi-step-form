aebc1ad5e519e96bb853698cd4430eef8e7ab980
Pinki-851 <77503524+Pinki-851@users.noreply.github.com>
2023-08-18 09:59:44 +0530

    Revert "Api setup"

--- src/constants/app-links.ts
+++ src/constants/app-links.ts
@@ -1,8 +1,5 @@
 export const AppLink = {
-  SIGNUP: '/flow/signup',
   LOGIN: '/flow/login',
   forgot_pass_success: '/flow/forgot-password/successfully-change',
   forgot_pass: '/flow/forgot-password',
-  WELCOME: '/',
-  VERIFY_EMAIL: '/flow/verify-email',
 };

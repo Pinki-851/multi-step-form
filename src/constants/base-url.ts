46d1e1e1c7c75d4cec39b8dd3eeca8c371752fae
Pinki-851 <Pinkisaroj851@outlook.com>
2023-08-19 23:52:35 +0530

    ENV VERIABLE

--- src/constants/base-url.ts
+++ src/constants/base-url.ts
@@ -1,5 +1,5 @@
-export const MONGOURL = process.env.MONGO_URL;
-export const TOKEN_SECRET = process.env.TOKEN_SECRET;
+export const MONGOURL = process.env.NEXT_PUBLIC_MONGO_URL;
+export const TOKEN_SECRET = process.env.NEXT_PUBLIC_TOKEN_SECRET;
 export const USER_NAME = process.env.NEXT_PUBLIC_NODE_USER;
 export const USER_PASSWORD = process.env.NEXT_PUBLIC_NODE_PASS;
 export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
